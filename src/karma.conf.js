f59e8397781ae2db450d2bcb254233b27f66e0e0
dcarballob01 <dcarballob01@gmail.com>
2019-02-23 21:12:05 +0100

    travis.yml - V4

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -21,12 +21,6 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
-	customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
