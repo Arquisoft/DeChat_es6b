d3bc2a8ade231525731a74f2b5fc234cf8aa8691
GomezIvann <UO258748@uniovi.es>
2019-03-30 18:09:09 +0100

    testing

---
+++ hellocucumber/features/step_definitions/PO_App.ts
@@ -0,0 +1,21 @@
+import { browser, by, element, until } from 'protractor';
+
+export class AppPage {
+
+  public navigateTo() {
+    return browser.get('/');
+    //return browser.get('http://localhost:4200/ ');
+  }
+
+  public enterSearchInput(text: string) {
+    return element(by.css('input[aria-label="search"]'))
+      .sendKeys(text);
+  }
+
+  public getSearchResultItems() {
+    const condition = until.elementsLocated(by.css('.search-results .search-result-item'));
+
+    return browser.wait(condition, 5000);
+  }
+
+}
