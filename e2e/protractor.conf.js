9ce3acb5cb3dcd792f64ad2eb012126e31f859c7
dcarballob01 <dcarballob01@gmail.com>
2019-04-01 21:55:00 +0200

    Error in the execution of ng e2e fixed

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,26 +1,25 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-//const { SpecReporter } = require('jasmine-spec-reporter');
-
 exports.config = {
   allScriptsTimeout: 11000,
-  specs: ['./src/features/**/*.feature'],
+  specs: ["./src/features/**/*.feature"],
   capabilities: {
-    'browserName': 'chrome'
+    "browserName": "chrome",
+    "chromeOptions": {
+      "args": ["--headless", "--no-sandbox"]
+    }
+  },
+  directConnect: true,
+  baseUrl: "http://localhost:4200/",
+  framework: "custom",
+  frameworkPath: require.resolve("protractor-cucumber-framework"),
+  onPrepare() {
+    require("ts-node").register({
+      project: require("path").join(__dirname, "./tsconfig.e2e.json")
+    });
   },
   cucumberOpts: {
-   require: ['./src/steps/**/*.steps.ts'],
- },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  framework: 'custom',
-  frameworkPath: require.resolve('protractor-cucumber-framework')
-  /**jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
+    require: ["./src/steps/**/*.steps.ts"],
   },
-  */
-
 };
---
+++ e2e/src/steps/app.steps.ts
@@ -0,0 +1,20 @@
+import { Given, Then, When, Before } from 'cucumber';
+import { expect } from 'chai';
+
+import { AppPage } from '../pages/app.po';
+
+let page: AppPage;
+
+Before(() => {
+  page = new AppPage();
+});
+
+Given('I want to go to the home view',
+    () => page.navigateTo());
+
+When('I do nothing',
+	() => {});
+
+Then('I should see the title',
+	() => page.getHomeText()
+	.then(elems => expect(elems.length).to.be.greaterThan(0)));
