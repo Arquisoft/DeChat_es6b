ed2acef053678e631cb09235c0fabbf0e3d5ae8e
GomezIvann <UO258748@uniovi.es>
2019-04-11 13:16:59 +0200

    Issue #42 first version

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -20,6 +20,7 @@ export class ChatComponent implements OnInit, AfterViewChecked {
   defaultImage = "assets/images/default.jpg";
   selectedChatChannel: ChatChannel; 
   myProfile: Participant;
+  allActiveChats: ChatChannel[]; /* for recovering from search */ 
 
   constructor(private chatService: ChatService, private rdf: RdfService, private auth: AuthService) {
   }
@@ -163,6 +164,10 @@ export class ChatComponent implements OnInit, AfterViewChecked {
     return this.chatService.chatChannels;
   }
 
+  getAllActiveChats(): ChatChannel[] {
+    return this.allActiveChats;
+  }
+
   async addNewChatChannel() {
     const inputElement: HTMLInputElement = document.getElementById('input_add_webid') as HTMLInputElement;
     const webid: string = inputElement.value;
@@ -172,22 +177,38 @@ export class ChatComponent implements OnInit, AfterViewChecked {
     }
   }
   
+  search() {
+    const inputSearch: HTMLInputElement = document.getElementById('input_search') as HTMLInputElement;
+    const name: string = inputSearch.value;
+
+    this.searchChat(name);
+  }
+
+  /**
+   * Muestra el botón go_back y oculta el trigger cuando gana el foco input_search
+   */
+  hideTriggerShowGoBack(){
+    const trigger: HTMLInputElement = document.getElementsByClassName('trigger').item(0) as HTMLInputElement;
+    trigger.hidden=true;
+    const go_back: HTMLInputElement = document.getElementsByClassName('go_back').item(0) as HTMLInputElement;
+    go_back.hidden=false;
+  }
+
   /**
    * Método que busca un chat según el texto introducido en el input_search en la lista de chats activos 
    * NO es sensible a mayúsculas y minúsculas
    */
-  search() {
-    const inputElement: HTMLInputElement = document.getElementById('input_search') as HTMLInputElement;
-    const name: string = inputElement.value;
-
+  private searchChat(name: string) {
     var newChatChannels: ChatChannel[] = new Array();
     for (let channel of this.chatService.chatChannels) {
       if ( channel.title.toString().toLowerCase() === name.toLowerCase()  || channel.title.toString().toLowerCase().includes(name.toLowerCase()) )
         newChatChannels.push(channel);
     }
 
-    if (newChatChannels.length!=0) 
+    if (newChatChannels.length!=0) {
+      this.allActiveChats = this.chatService.chatChannels; // guardamos todos los canales activos para una posible recuperación
       this.chatService.setChatChannels(newChatChannels);
+    }
     else
       alert("No se han encontrado coincidencias con ningún chat activo.");
   }
