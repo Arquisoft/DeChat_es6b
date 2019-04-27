a98f8b1436bebdc4992f76d09527b5b12876bddd
dcarballob01 <dcarballob01@gmail.com>
2019-04-28 01:28:40 +0200

    Adapted some tests of Ivan to Karma

--- src/app/services/chat.service.spec.ts
+++ src/app/services/chat.service.spec.ts
@@ -3,23 +3,24 @@ import { Toast, ToastrModule } from 'ngx-toastr';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { ChatService } from '../services/chat.service';
+import { RdfService } from './rdf.service';
 import * as assert from 'assert';
 
 
 describe('ChatService', () => {
-
-  let credentials = {
-    "idp"      : "https://solid.community",
-    "username" : "dechates6b",                  
-    "password" : "dechates6b",
-    "base"     : "https://dechates6b.solid.community",
-    "test"     : "/public/test/"   
-  }
-
   let timeout = 2100;
-  let webID = "https://pruebases6b.solid.community/profile/card#me";
-  let mywebID = "https://gomezivann.inrupt.net"
   let chatService: ChatService;
+  let rdfService: RdfService;
+
+  let me = {
+    "base": "https://dechates6b.solid.community",
+    "webid": "https://dechates6b.solid.communityprofile/card#me"
+  };
+
+  let other = {
+    "base": "https://pruebases6b.solid.community",
+    "webid": "https://pruebases6b.solid.community/profile/card#me"
+  };
 
   beforeEach(async( async () => {
     TestBed.configureTestingModule({
@@ -29,7 +30,11 @@ describe('ChatService', () => {
     })
     .compileComponents();
 
+    rdfService = TestBed.get(RdfService);
     chatService = TestBed.get(ChatService);
+
+    chatService.webid = me.webid;
+    chatService.uri = me.base;
   }));
 
   it('should be created', () => {
@@ -38,9 +43,24 @@ describe('ChatService', () => {
 
   it ('new chat', async function() {
     // this.timeout(timeout);
-    // let channel = await chatService.createNewChatChannel(webID, "chat de pruebas");
-    // assert.notEqual(chatService.getRdfService().readFile(mywebID+"/private/dechat_es6b/"+channel.id), null);
+    let channel = await chatService.createNewChatChannel(other.webid, "chat de pruebas");
+    assert.notEqual(await chatService.getRdfService().readFile(me.base + "/private/dechat_es6b/" + channel.id), null);
+
+    // Remove new channel
+    await rdfService.deleteFile(me.base + "/private/dechat_es6b/" + channel.id);
+    assert.equal(await rdfService.readFile(me.base + "/private/dechat_es6b/" + channel.id), null);
   });
+
+  it ('search chat by web id', async function() {
+    // this.timeout(timeout);
+    await chatService.createNewChatChannel(other.webid, "chat de pruebas");
+    let channel = chatService.searchChatChannelByParticipantWebid(other.webid);
+    assert.notStrictEqual(channel.title.toString().toLowerCase(), "pruebases6b"); // Use the account name
+
+    // Remove new channel
+    await rdfService.deleteFile(me.base + "/private/dechat_es6b/" + channel.id);
+    assert.equal(await rdfService.readFile(me.base + "/private/dechat_es6b/" + channel.id), null);
+  }); 
 
 });
 
